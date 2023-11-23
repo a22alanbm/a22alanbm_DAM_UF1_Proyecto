02e6464462e6a953389f0e59bacfec69173c08ea
a22alanbm <a22alanbm@iessanclemente.net>
2023-11-23 14:37:18 +0100

    Hice cosas, la toolbar no va

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.uf1_proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainSheetViewFragment.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainSheetViewFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 
 
 class MainSheetViewFragment : Fragment() {
@@ -15,6 +20,15 @@ class MainSheetViewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        val toolbar = view?.findViewById<MaterialToolbar>(R.id.toolbar)
+        val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.sheet_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawerLayout = view?.findViewById<DrawerLayout>(R.id.drawer_layout)
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawerLayout)
+        val appBarConfiguration = builder.build()
+        toolbar?.setupWithNavController(navController, appBarConfiguration)
         return inflater.inflate(R.layout.fragment_main_sheet_view, container, false)
     }
 
