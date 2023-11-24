6a2925ac0666d67a680201dfe2e419bf40fad5b5
a22alanbm <a22alanbm@iessanclemente.net>
2023-11-24 14:35:18 +0100

    He hecho cosas y creado dos actividades, a ver si en el futuro funciona

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/CharacterGalleryFragment.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/CharacterGalleryFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.uf1_proyecto.databinding.FragmentCharacterGalleryBinding
 
 class CharacterGalleryFragment : Fragment() {
-
-
+    lateinit var binding_: FragmentCharacterGalleryBinding
+    val binding = binding_!!
+    val view = binding.root
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        binding.button.setOnClickListener {
+            //go to character editor activity
+
+        }
+
         return inflater.inflate(R.layout.fragment_character_gallery, container, false)
     }
 
