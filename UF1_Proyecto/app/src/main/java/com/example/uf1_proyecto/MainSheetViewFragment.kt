429b0ce09aec4e97dbcbe6a03c5d908de4096071
a22alanbm <a22alanbm@iessanclemente.net>
2023-11-24 09:34:03 +0100

    no he hecho mucho

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainSheetViewFragment.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainSheetViewFragment.kt
@@ -3,11 +3,15 @@ package com.example.uf1_proyecto
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -31,5 +35,14 @@ class MainSheetViewFragment : Fragment() {
         toolbar?.setupWithNavController(navController, appBarConfiguration)
         return inflater.inflate(R.layout.fragment_main_sheet_view, container, false)
     }
+    fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = view?.findNavController(R.id.sheet_container_view)
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item)
+    }
 }
