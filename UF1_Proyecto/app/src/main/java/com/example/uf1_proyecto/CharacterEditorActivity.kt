19b43dc6d31bf62e8e37a63e6e9a4c03e2dd2c6a
a22alanbm <a22alanbm@iessanclemente.net>
2023-12-02 16:54:57 +0100

    onOptionsItemSelected no funciona para la bottom navigation Bar, sino que para los menu de tres puntitos

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/CharacterEditorActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/CharacterEditorActivity.kt
@@ -1,21 +1,17 @@
 package com.example.uf1_proyecto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import androidx.appcompat.app.ActionBar.DISPLAY_USE_LOGO
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.size
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.example.uf1_proyecto.R.color.moraro
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
